62fe914ada096c72cc2ebed003b658bc310bcb00
djpb10 <64172903+djpb10@users.noreply.github.com>
2025-05-16 21:56:30 -0400

    Update lexer.py
    Se agregaron los tokens de la forma correcta, se agregaron punciones adicionales de lectura de tokens y se preparó un pequeño ejemplo del funcionamiento del programa

--- lexer.py
+++ lexer.py
@@ -8,39 +8,119 @@ import ply.lex as Lex
 
 def main():
 
-    tokens = {
-        "if" : "TkIf" ,
-        "end" : "TkEnd" ,
-        "while" : "TkWhile",
-        "{" : "TkOBlock" ,
-        "}" : "TkCBlock" ,
-        ". ." : "TkSoForth" ,
-        "," : "TkComma" ,
-        "(" : "TkOpenPar" ,
-        ")" : "TkClosePar" ,
-        ":=" : "TkAsig" ,
-        ";" : "TkSemicolon" ,
-        "-->" : "TkArrow" ,
-        "[]" : "TkGuard" ,
-        "+" : "TkPlus" ,
-        "-" : "TkMinus" ,
-        "*" : "TkMult" ,
-        "or" : "TkOr" ,
-        "and" : "TkAnd" ,
-        "!" : "TkNot" ,
-        "<" : "TkLess" ,
-        "<=" : "TkLeq" ,
-        ">=" : "TkGeq" ,
-        ">" : "TkGreater" ,
-        "==" : "TkEqual" ,
-        "<>" : "TkNEqual" ,
-        "[" : "TkOBracket" ,
-        "]" : "TkCBracket" ,
-        ":" : "TkTowPoints" ,
-        "." : "TkApp"   
-        }
+    tokens = (
+        "TkIf" ,
+        "TkEnd" ,
+        "TkWhile" ,
+        "TkOBlock" ,
+        "TkCBlock" ,
+        "TkSoForth" ,
+        "TkComma" ,
+        "TkOpenPar" ,
+        "TkClosePar" ,
+        "TkAsig" ,
+        "TkSemicolon" ,
+        "TkArrow" ,
+        "TkGuard" ,
+        "TkPlus" ,
+        "TkMinus" ,
+        "TkMult" ,
+        "TkOr" ,
+        "TkAnd" ,
+        "TkNot" ,
+        "TkLess" ,
+        "TkLeq" ,
+        "TkGeq" ,
+        "TkGreater" ,
+        "TkEqual" ,
+        "TkNEqual" ,
+        "TkOBracket" ,
+        "TkCBracket" ,
+        "TkTowPoints" ,
+        "TkApp" ,
+        "TkNum"
+    )
+
+    # tokens sencillos
+
+    t_TkIf = "if"
+    t_TkEnd = "end"
+    t_TkWhile = "while"
+    t_TkOBlock = r"\{"
+    t_TkCBlock = r"\}"
+    t_TkSoForth = r"\. \."   #revisar esto bien
+    t_TkComma = r"\,"       #no se si sea necesario de esta forma
+    t_TkOpenPar = r"\("
+    t_TkClosePar = r"\)"
+    t_TkAsig = r"\:\="
+    t_TkSemicolon = r"\;"
+    t_TkArrow = r"\-\-\>"
+    t_TkGuard = r"\[\]"
+    t_TkPlus = r"\+"
+    t_TkMinus = r"\-"
+    t_TkMult = r"\*"
+    t_TkOr = "or"
+    t_TkAnd = "and"
+    t_TkNot = r"\!"
+    t_TkLess = r"\<"
+    t_TkLeq = r"\<\="
+    t_TkGeq = r"\>\="
+    t_TkGreater = r"\>"
+    t_TkEqual = r"\=\="
+    t_TkNEqual = r"\<\>"
+    t_TkOBracket = r"\["
+    t_TkCBracket = r"\]"
+    t_TkTowPoints = r"\:"
+    t_TkApp = r"\."
+
+    # tokens especiales
+
+    def t_TkNum( t ):
+        r"\d+"
+        t.value = int(t.value)
+        return t
+
+    # tokens ignorados
+
+    t_ignore = " \t"
+
+    # conteo de lineas 
+    
+    def t_newline( t ):
+        r"\n+"
+        t.lexer.lineno += len(t.value)
+
+    # manejo de errores 
+    
+    def t_error( t ):
+        print( " Error ")
+        t.lexer.skip(1)
 
 
+    # llamada al contructor lexico
+
+    lexer = Lex.lex()
+
+
+
+    # dato de prueba
+
+    prueba = """
+    3 + 4 * 10 ( and while )
+    + -20 *2      
+    """
+
+    # entrada del dato
+
+    lexer.input( prueba )
+
+    # procesamiento del dato
+
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break
+        print( tok )
 
 
 
